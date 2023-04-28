5e989ee75e3e2698027575b0226175d6b5c6489c
Christian Cabrera <ccabrera.cd@gmail.com>
2023-04-27 23:28:01 -0300

    tercera preentrega

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("principal")
+const ProductManager = require("./ProductManager")
+
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.get("/", (req, res)=>{
+    let productNew = new ProductManager("./products.json")
+    const resp = productNew.getProducts()
+    resp.then(pr => {
+        console.log(pr)
+        let productos = JSON.parse(pr, null, 2) 
+        res.send({data:productos, menssage:"OK"})
+    }).catch(err => {
+        console.log(err)
+    })
+})
+
+app.get("/product/:id", (req, res) => {
+    let productNew = new ProductManager("./products.json")
+    let id = req.params.id
+    const resp = productNew.getProductById(id)
+    resp.then(pr => {
+        console.log(pr)
+        let productos = JSON.parse(pr, null, 2)
+        res.send({data:productos, menssge:"OK"})
+    }).catch(err => {
+        console.log(err)
+    })
+})
+
+app.listen(8080, () =>{
+    console.log("Server run on port 8080")
+}) 
